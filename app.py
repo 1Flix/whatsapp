a11002a6070125287856420deec4873090508fce
1Flix <99018674+1Flix@users.noreply.github.com>
2024-12-10 21:37:05 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def run_script():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(hot='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
